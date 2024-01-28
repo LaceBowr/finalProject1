d6c53fe4f591eae3b32b3e30c11488957dd994df
Layci <layciebowers@gmail.com>
2024-01-27 18:46:42 -0700

    after the python mock db integrationn

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # finalProject1
 python for the finalProject for graduation from BYUI
+
+
+NOTES on building and running the docker:
+
+fire up shell somewhere to do this
+
+from the directory with the Dockerfile (this directory) run:
+
+docker build . -t finalProject
+ 
+and then run the container in interactive mode:
+
+docker run -it -p 5000:5000 finalproject
+
+For that to work the build command must have succeeded, otherwise you are running an old version of the container.  So make sure you save your docker file and that the build command runs first
--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from base64 import b64encode
+import argparse
+import mysql.connector
 
 app = Flask(__name__)
-#CORS(app, origins='http://localhost:8400') 
-#CORS(app, resources={r"/api/*": {"origins": "http://localhost:8400"}})
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+db_connection = None
 
+def get_db_connection():
+    if db_connection is None:
+        # TODO get your actual db connection string    
+        db_connection = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "Johnisageek#1"
+        )
+    return db_connection
+ 
 @app.route('/car/makes', methods=['GET'])
 @cross_origin()
 def get_cars():
-    return jsonify(["Chevrolet","GMC", "Saturn", "Ford", "Dodge", "Toyota"])
+    db = get_db_connection()
+    db.cursor.execute("select * from get_cars_by_make_and_model() db_connection;")
+        # jsonify and return results
+    return jsonify(get_cars_by_make_and_model)
+    # TODO get the value from the cursor, check for successful execution
+    
+    #return jsonify(["Chevrolet","GMC", "Saturn", "Ford", "Dodge", "Toyota"])
 
 @app.route('/car/<make>/<model>', methods=['GET'])
 @cross_origin()
@@ -27,8 +45,8 @@ def get_cars_by_make_and_model(make, model):
         if model == "Ion":
             model_years = [2004]
     elif make == "Ford":
-        if model == "Windstar-LX":
-            model_years = [2002]
+        if model == "Mustang":
+            model_years = [1998]
     elif make == "Dodge":
         if model == "Intrepid":
             model_years = [1994]
@@ -45,10 +63,10 @@ def get_cars_by_make_and_model(make, model):
 def get_cars_by_make_and_model_and_year(make, model, year):
     car_data = [{
         'make': make,
-        'images': ["images\driver-side-image.png", "images\passenger-side-image.png", "images\\front-view-image.png", "images\\rear-view-image.png"],
+        'images': ["mustang_front", "mustang_rear", "mustang_driver", "mustang_passenger"],
         'model': model,
         'year': year,
-        'color': 'blue',
+        'color': 'black',
         'mileage': 99000,
         'notes': ''
     }]
@@ -66,7 +84,7 @@ def get_car_models(make):
     elif make == "Saturn":
         car_models = ["Ion", "SL2", "L2"]
     elif make == "Ford":
-        car_models = ["Windstar-LX", "F-250", "Bronco Top", "Taurus", "Mustang", "Ranchero", "F-250"]
+        car_models = ["Windstar", "F-250", "Bronco Top", "Taurus", "Mustang", "Ranchero", "F-250"]
     elif make == "Dodge":
         car_models = ["Intrepid", "Grand Caravan", "Aries"]
     elif make == "Toyota":
@@ -117,50 +135,43 @@ def add_car():
         }
         return jsonify(response)
 
+@app.route('/getimage/<imagename>')
+@cross_origin()
+def get_image(imagename):
+    try:
+        image_path = f'images/{imagename}.png'
+        # Read the image file
+        with open(image_path, "rb") as image_file:
+            # Convert the image to base64
+            base64_data = b64encode(image_file.read()).decode('utf-8')
 
-'''
-@app.route('/cars', methods=['GET'])
-def get_cars():
-    all_cars = {
-        'make': 'Chevrolet',
-        'model': 'Tahoe',
-        'year': 2008,
-        'color': 'Black',
-        'mileage': 15000;
-
-        'make': 'Dodge',
-        'model': 'Grand Caravan',
-        'year': 2002,
-        'color': 'white',
-        'mileage': 25000;
-
-        'make': 'Mecury',
-        'model': 'Tracer',
-        'year': 1990,
-        'color': 'pearl',
-        'mileage': 70000
-    }
-    return jsonify(all_cars)
-
-@app.route('/cars/<make>', methods=['GET'])
-def list_of_car_makes_details(make):
-    find_makes_list = {
-        if make = 'Chevrolet'
-            apend.car_data((car_data) in list_of_car_makes)
-        elif make = 'GMC'
-            apend.car_data((car_data) in list_of_car_makes)
-        elif make = 'Saturn'
-            apend.car_data((car_data) in list_of_car_makes)
-        elif make = 'Ford'
-            apend.car_data((car_data) in list_of_car_makes)
-        elif make = 'Dodge'
-            apend.car_data((car_data) in list_of_car_makes)
-        else:
-            make = []
-            apend.car_data((car_data))
+        # Create a JSON response with the base64 data
+        json_response = {
+            "success": True,
+            "message": "Image converted to base64 successfully",
+            "base64_data": base64_data
         }
-        return (list_of_car_makes_details)
-            print((car_data) in find_makes_list)
-'''
+
+    except FileNotFoundError:
+        json_response = {
+            "success": False,
+            "message": "File not found. Please provide a valid file path."
+        }
+    except Exception as e:
+        json_response = {
+            "success": False,
+            "message": f"An error occurred: {str(e)}"
+        }
+
+    return json_response
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Flask App with Port Option')
+    parser.add_argument('--port', type=int, default=5000, help='Port number to run the Flask app on')
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    args = parse_args()
+    # Listen on all network interfaces (0.0.0.0) and the specified port
+    app.run(debug=True, host='0.0.0.0', port=args.port)
