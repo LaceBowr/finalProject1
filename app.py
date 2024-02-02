1ec7d6d082bbec9d6a4082bfc155e750aa2ef81a
Layci <layciebowers@gmail.com>
2024-02-01 22:16:23 -0700

    week4 python and DB integration

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS, cross_origin
 from base64 import b64encode
 import argparse
 import mysql.connector
+import os
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,87 +12,86 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 db_connection = None
 
 def get_db_connection():
+    global db_connection
     if db_connection is None:
         # TODO get your actual db connection string    
         db_connection = mysql.connector.connect(
-            host = "localhost",
-            user = "root",
-            password = "Johnisageek#1"
+            host = "10.70.1.211",
+            user = "serviceaccount",
+            password = "test1234",
+            db = "radical_wreckage_schema"
         )
     return db_connection
- 
+
+def perform_sql_select_return_as_list(select_query): 
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute(select_query)
+    results = cursor.fetchall()
+    result_list = []
+    for row in results:
+        result_list.append(row[0])
+    cursor.close()
+    return result_list
+
+# For images we are selecting 4 items, so this function return 4 items
+def perform_sql_image_query(select_query): 
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute(select_query)
+    row = cursor.fetchone()
+    result_list = []
+    result_list.append(row[0])
+    result_list.append(row[1])
+    result_list.append(row[2])
+    result_list.append(row[3])
+    cursor.close()
+    return result_list
+
 @app.route('/car/makes', methods=['GET'])
 @cross_origin()
 def get_cars():
-    db = get_db_connection()
-    db.cursor.execute("select * from get_cars_by_make_and_model() db_connection;")
-        # jsonify and return results
-    return jsonify(get_cars_by_make_and_model)
-    # TODO get the value from the cursor, check for successful execution
-    
-    #return jsonify(["Chevrolet","GMC", "Saturn", "Ford", "Dodge", "Toyota"])
+    query_str = 'select distinct make_name from cars join makes on cars.makes_id=makes.id;'   
+    result_list = perform_sql_select_return_as_list(query_str)
+    return jsonify(result_list)  
 
 @app.route('/car/<make>/<model>', methods=['GET'])
 @cross_origin()
 def get_cars_by_make_and_model(make, model):
-    model_years = []
-    if make == "Chevrolet":
-        if model == "Cavalier":
-            model_years = [1978, 1997, 2001, 2010]
-    elif make == "GMC":
-        if model == "k-1500":
-            model_years = [1986]
-    elif make == "Saturn":
-        if model == "Ion":
-            model_years = [2004]
-    elif make == "Ford":
-        if model == "Mustang":
-            model_years = [1998]
-    elif make == "Dodge":
-        if model == "Intrepid":
-            model_years = [1994]
-    elif make == "Toyota":
-        if model == "Avalon":
-            model_years = [2000]
-    else:
-        if model == "":
-            model_years = []
-    return jsonify(model_years)
+    query_str = f'select year from cars join makes on cars.makes_id=makes.id where make_name="{make}" and model="{model}" order by year asc;'
+    result_list = perform_sql_select_return_as_list(query_str)
+    return jsonify(result_list) 
 
 @app.route('/car/<make>/<model>/<year>', methods=['GET'])
 @cross_origin()
 def get_cars_by_make_and_model_and_year(make, model, year):
+    query_str = f'select driver_side_image, passenger_side_image, rear_view_image, front_view_image  from cars join makes on cars.makes_id=makes.id where make_name="{make}" and model="{model}" and year={year};'
+    images = perform_sql_image_query(query_str)
+    print("Images:", images)
     car_data = [{
         'make': make,
-        'images': ["mustang_front", "mustang_rear", "mustang_driver", "mustang_passenger"],
+        'images': images,
         'model': model,
         'year': year,
-        'color': 'black',
-        'mileage': 99000,
+        'color': '',
+        'mileage': 0,
         'notes': ''
     }]
     return jsonify(car_data)
 
+@app.route('/car/models/<make>/<year>')
+@cross_origin()
+def get_car_year(makes_id, make_name, year):
+    yearstr = f'select * from cars join makes on cars.makes_id="{makes_id}" where makes make_name="{make_name}" and year="{year}" order by year asc;'
+    result_list = perform_sql_select_return_as_list(yearstr)
+    return jsonify(result_list)
 
 @app.route('/car/models/<make>')
 @cross_origin()
 def get_car_models(make):
-    car_models = []
-    if make == "Chevrolet":
-        car_models = ["Cabriolet", "Cavalier", "Camero", "Silverado"]
-    elif make == "GMC":
-        car_models = ["k-1500"]
-    elif make == "Saturn":
-        car_models = ["Ion", "SL2", "L2"]
-    elif make == "Ford":
-        car_models = ["Windstar", "F-250", "Bronco Top", "Taurus", "Mustang", "Ranchero", "F-250"]
-    elif make == "Dodge":
-        car_models = ["Intrepid", "Grand Caravan", "Aries"]
-    elif make == "Toyota":
-        car_models = ["Camry", "Avalon"]
-    else:
-        car_models = ["no cars (in database) of this make"]
-    return jsonify(car_models)
+    querystr = f'select distinct model from cars join makes on cars.makes_id=makes.id where makes.make_name="{make}" order by model asc;'
+    result_list = perform_sql_select_return_as_list(querystr)
+    return jsonify(result_list)  
 
 @app.route('/add_car', methods=['POST'])
 @cross_origin()
@@ -140,6 +140,12 @@ def add_car():
 def get_image(imagename):
     try:
         image_path = f'images/{imagename}.png'
+        image_type = "png"
+        if not os.path.exists(image_path):
+            image_path = f'images/{imagename}.jpg'
+            if not os.path.exists(image_path):
+                image_path = f'images/{imagename}.jpeg'
+            image_type = "jepg"
         # Read the image file
         with open(image_path, "rb") as image_file:
             # Convert the image to base64
@@ -149,7 +155,8 @@ def get_image(imagename):
         json_response = {
             "success": True,
             "message": "Image converted to base64 successfully",
-            "base64_data": base64_data
+            "base64_data": base64_data,
+            "image_type": image_type 
         }
 
     except FileNotFoundError:
